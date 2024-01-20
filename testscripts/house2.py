a3aa19c02c2cad8aa02296719eb398f56b36f5ca
Sam <samlee2015jp@gmail.com>
2024-01-20 23:23:04 +0900

    add

--- testscripts/house2.py
+++ testscripts/house2.py
@@ -1,12 +1,14 @@
 name = input("What's your name? ")
 
 match name:
-    case "Harry":
+    case "Harry" | "Hermione" | "Ron":
         print("Gryffindor")
-    case "Hermione":
-        print("Gryffindor")
-    case "Ron":
-        print("Gryffindor")
+    #case "Harry":
+    #    print("Gryffindor")
+    #case "Hermione":
+    #    print("Gryffindor")
+    #case "Ron":
+    #    print("Gryffindor")
     case "Draco":
         print("Slytherin")
     case _:

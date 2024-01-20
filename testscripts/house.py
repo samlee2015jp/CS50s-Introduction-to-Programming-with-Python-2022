763e33bb8131f077f56323e67c7840c69733c829
Sam <samlee2015jp@gmail.com>
2024-01-20 23:20:21 +0900

    add

---
+++ testscripts/house.py
@@ -0,0 +1,14 @@
+name = input("What's your name? ")
+
+if name == "Harry" or name == "Hermione" or name == "Ron":
+    print("Gryffindor")
+#if name == "Harry":
+#    print("Gryffindor")
+#elif name == "Hermione":
+#    print("Gryffindor")
+#elif name == "Ron":
+#    print("Gryffindor")
+elif name == "Draco":
+    print("Slytherin")
+else:
+    print("Who?")

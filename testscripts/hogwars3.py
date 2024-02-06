bb71bd789100b0c7e33daf4ae9b9a3ac2927ec7d
Sam <samlee2015jp@gmail.com>
2024-02-06 20:07:00 +0900

    add

---
+++ testscripts/hogwars3.py
@@ -0,0 +1,11 @@
+students = [
+    {"name": "Hermione", "house": "Gryffindor", "patronus": "Otter"},
+    {"name": "Harry", "house": "Gryffindor", "patronus": "Stag"},
+    {"name": "Ron", "house": "Gryffindor", "patronus": "Jack Russell terrier"},
+    {"name": "Draco", "house": "Slytherin", "patronus": None}
+]
+
+print("-" * 50)
+
+for student in students:
+    print(student["name"], student["house"], student["patronus"])
